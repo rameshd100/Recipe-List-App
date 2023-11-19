b890fadd2f9378144cd0304bd111d8551f861889
ramesh dahal <rameshdahal2013@gmail.com>
2023-11-19 15:28:58 -0800

    Add RecipeTabView as the starting point for the app

---
+++ Recipe List App/Views/RecipeTabView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeTabView.swift
+//  Recipe List App
+//
+//  Created by Ramesh  Dahal  on 11/19/23.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeTabView()
+}
